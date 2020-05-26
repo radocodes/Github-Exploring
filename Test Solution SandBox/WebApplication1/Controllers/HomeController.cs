5b5ebe7edc7e80d362b5603cd29474451c5ecfec
up_radoslavyordanov <up.radoslavyordanov@gmail.com>
2020-05-26 12:36:47 +0300

    my test - C

--- Test Solution SandBox/WebApplication1/Controllers/HomeController.cs
+++ Test Solution SandBox/WebApplication1/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace WebApplication1.Controllers
             var test = 1;
             var myTest = 5;
             var myTestA = "A";
+            var myTestB = "B";
+            var myTestC = "C";
 
             return View();
         }
