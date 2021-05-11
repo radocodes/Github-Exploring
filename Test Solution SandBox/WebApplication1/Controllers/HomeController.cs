44f0a95d1eab16b0e4f9c5f7a9fb6056d759a10c
up_radoslavyordanov <up.radoslavyordanov@gmail.com>
2021-05-11 17:45:43 +0300

    Revert "Test Removing local commit"
    This reverts commit 2451307fa0a23c1da28a9e2238df9757ccd60811.

--- Test Solution SandBox/WebApplication1/Controllers/HomeController.cs
+++ Test Solution SandBox/WebApplication1/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page..";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
